38bbc425b27cf5eacccfc1c6500d952cb55b3c65
Yurii Myronov <xmyron00@stud.fit.vutbr.cz>
2023-10-16 21:03:21 +0200

    Change structure for api

--- BackEnd/authorization/api/services.py
+++
@@ -1,7 +0,0 @@
-from authorization.models import Permission
-
-
-def read_permissions():
-    """Reading from db Permission levels"""
-
-    return Permission.objects.all()
--- BackEnd/authorization/api/view.py
+++ BackEnd/authorization/api/view.py
@@ -1,13 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from authorization.api.services import read_permissions
 from authorization.api.serializators import PermissionSerializer
+from authorization.models import Permission
 
 
 @api_view(['GET'])
 def get_permissions(request):
     """Get all permission levels"""
 
-    permissions = read_permissions()
+    permissions = Permission.objects.all()
     serializator = PermissionSerializer(permissions, many=True)
     return Response(serializator.data)
