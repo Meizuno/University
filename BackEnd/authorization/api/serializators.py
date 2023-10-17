7edd9234edad3cbc78263a8dfd2d323dbdd3c021
Yurii Myronov <xmyron00@stud.fit.vutbr.cz>
2023-10-17 21:49:49 +0200

    Create user api

--- BackEnd/authorization/api/serializators.py
+++ BackEnd/authorization/api/serializators.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from authorization.models import Permission
+from authorization.decorators import handle_error
+from authorization.models import Permission, User
 
 
 class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Permission
         fields = "__all__"
+
+
+class UserSerializator(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True, max_length=150)
+    first_name = serializers.CharField(required=True, max_length=150)
+    last_name = serializers.CharField(required=True, max_length=150)
+    permission_id = serializers.IntegerField(
+        required=True,
+        help_text="ID of user's permission.\n Get enum on 'api/auth/permissions/'"
+    )
+
+    @handle_error
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
--- BackEnd/authorization/decorators.py
+++ BackEnd/authorization/decorators.py
@@ -1,4 +1,4 @@
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, APIException
 
 
 def login_required(func):
@@ -6,4 +6,13 @@ def login_required(func):
         if not request.user.is_authenticated:
             raise PermissionDenied("Permissions denied.")
         return func(request, *args, **kwargs)
+    return wrapper
+
+
+def handle_error(func):
+    def wrapper(request, *args, **kwargs):
+        try:
+            return func(request, *args, **kwargs)
+        except Exception as ex:
+            raise APIException(detail="An error occurred: " + str(ex))
     return wrapper
