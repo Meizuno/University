885f151b2f474136d05497c0c6255560a444e0d2
Yurii Myronov <xmyron00@stud.fit.vutbr.cz>
2023-11-07 20:58:49 +0100

    Componen scheduleCell

---
+++ FrontEnd/university-schedule/src/components/ScheduleCell.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="practice" :style="outside">
+        <div :style="inside">
+            <p :style="title">Subject</p>
+            <p class="var">ITU</p>
+        </div>
+        <div :style="inside">
+            <p :style="title">Room</p>
+            <p class="var">105</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    outsideColor: {
+      type: String,
+      default: 'red'
+    },
+    insideColor: {
+      type: String,
+      default: 'red'
+    },
+    titleColor: {
+      type: String,
+      default: 'white'
+    }
+  },
+  computed: {
+    outside() {
+      return {
+        backgroundColor: this.outsideColor
+      };
+    },
+    inside() {
+      return {
+        backgroundColor: this.insideColor
+      };
+    },
+    title() {
+      return {
+        color: this.titleColor
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.practice {
+    display: flex;
+    gap: 10px;
+    width: fit-content;
+    justify-content: center;
+    color: white;
+    padding: 15px 10px;
+    border-radius: 20px;
+}
+
+.practice > div {
+    text-align: center;
+    padding: 3px 10px;
+    border-radius: 10px;
+}
+
+.practice > div > p:first-child {
+    font-size: 12px;
+}
+
+.practice > div > p {
+    margin: 0;
+}
+
+.var {
+    font-size: 28px;
+}
+
+</style>
---
+++ FrontEnd/university-schedule/src/views/Scheduler.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ScheduleCell from '../components/ScheduleCell.vue'
+</script>
+
+<template>
+    <ScheduleCell outsideColor="#84D296" insideColor="#9DDBAB" titleColor="#E2F4E6"/>
+    <ScheduleCell outsideColor="#FFBB56" insideColor="#FFB13B" titleColor="#FFD89D"/>
+    <ScheduleCell outsideColor="#45BFFF" insideColor="#41B4F0" titleColor="#A2DAF8"/>
+    <ScheduleCell outsideColor="#717EEE" insideColor="#8993ED" titleColor="#C5CAF6"/>
+</template>
