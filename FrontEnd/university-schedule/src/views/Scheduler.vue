710a9d96ff4dc30ca424978c737947812e9c471d
Yurii Myronov <xmyron00@stud.fit.vutbr.cz>
2023-11-17 12:06:40 +0100

    Calendar

--- FrontEnd/university-schedule/src/views/Scheduler.vue
+++ FrontEnd/university-schedule/src/views/Scheduler.vue
@@ -10,18 +10,16 @@
                 :key="activity.id" 
                 :activity="activity" 
                 :rooms="rooms"
-                @update-activity="NewActivity"
+                @add-to-calendar="HandleAddToCalendar"
             />
         </div>
-        <div>
-            <ScheduleCell 
-                v-for="activity in activitiesResolved"
-                :key="activity.id"
-                :activity="activity"
-                :isScheduler="true"
-                @delete-activity="NewActivity"
-            />
-        </div>
+
+        <Calendar
+            ref="calendarRef"
+            v-if="dataForCalendarReceived"
+            :activities="activitiesResolved"
+            @remove-from-calendar="HandleRemoveFromCalendar"
+        />
     </main>
 </template>
 
@@ -30,13 +28,16 @@
 import Navigation from '../components/Navigation.vue';
 import ScheduleCell from '../components/ScheduleCell.vue';
 import NotResolvedCell from '../components/NotResolvedCell.vue';
+import Calendar from '../components/Calendar.vue';
 import axios from 'axios';
 
 export default {
+    emits: ["remove-from-calendar", "add-to-calendar", "add-to-calendar"],
     components: {
         Navigation,
         NotResolvedCell,
-        ScheduleCell
+        ScheduleCell,
+        Calendar
     },
     data() {
         return {
@@ -49,45 +50,55 @@ export default {
             activitiesNotResolved: [],
             activitiesResolved: [],
             rooms: [],
+
+            dataForCalendarReceived: false,
         };
     },
     methods: {
-        NewActivity() {
+        GetActivities() {
+            this.dataForCalendarReceived = false;
             axios.get('http://127.0.0.1:8000/api/activity')
             .then(response => {
                 let activities = response.data.data;
                 this.activitiesNotResolved = [];
                 this.activitiesResolved = [];
                 for (const activity of activities) {
-                    if (activity.date_time === null){
+                    if (activity.time === null){
                         this.activitiesNotResolved.push(activity);
                     }
                     else {
                         this.activitiesResolved.push(activity);
                     }
                 }
+                this.dataForCalendarReceived = true;
             })
             .catch(error => {
                 console.error('Error response for activity: ', error);
             });
+        },
+        HandleRemoveFromCalendar(activity) {
+            axios.delete(`http://127.0.0.1:8000/api/scheduler-activity/${activity.id}`)
+            .then(response => {
+                this.GetActivities();
+                this.$refs.calendarRef.UpdateCalendar();
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        },
+        HandleAddToCalendar(activity, data) {
+            axios.post(`http://127.0.0.1:8000/api/scheduler-activity/${activity.id}`, data)
+            .then(response => {
+                this.GetActivities();
+                this.$refs.calendarRef.UpdateCalendar();
+            })
+            .catch(error => {
+                console.error(error);
+            });
         }
     },
     mounted() {
-        axios.get('http://127.0.0.1:8000/api/activity')
-        .then(response => {
-            let activities = response.data.data;
-            for (const activity of activities) {
-                if (activity.date_time === null){
-                    this.activitiesNotResolved.push(activity);
-                }
-                else {
-                    this.activitiesResolved.push(activity);
-                }
-            }
-        })
-        .catch(error => {
-            console.error('Error response for activity: ', error);
-        });
+        this.GetActivities();
 
         axios.get('http://127.0.0.1:8000/api/room')
         .then(response => {
@@ -104,14 +115,14 @@ export default {
 <style scoped>
 
 .not-resolved-list {
+    position: relative;
     margin: 20px;
     background-color: white;
-    border-radius: 15px 15px 0px 0px;
+    border-radius: 15px;
     padding: 20px;
     display: flex;
+    flex-wrap: wrap;
     gap: 20px;
-    overflow-y: hidden;
-    overflow-x: auto;
 }
 
 </style>
